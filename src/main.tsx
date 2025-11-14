54f1b4a72381e25f4ee8ae82671ada4a81ef3242
Vipul0052 <vipulsharm969@gmail.com>
2025-11-15 00:52:44 +0530

    feat: add Vercel Analytics for tracking page views

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 
   import { createRoot } from "react-dom/client";
+  import { Analytics } from "@vercel/analytics/react";
   import App from "./App.tsx";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <>
+      <App />
+      <Analytics />
+    </>
+  );
   
