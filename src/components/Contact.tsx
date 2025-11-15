37508bde3527653bf30de597835e2e8f060b3fac
Vipul0052 <vipulsharm969@gmail.com>
2025-11-15 15:43:47 +0530

    fix: update Google Maps to show correct location - Near Mahadev Chauraha Dibai Bulandshahr

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -101,7 +101,7 @@ export function Contact() {
             {/* Google Maps */}
             <div className="bg-white rounded-3xl overflow-hidden shadow-lg border border-gray-100 h-64">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3496.830987!2d77.90!3d28.36!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDIxJzM2LjAiTiA3N8KwNTQnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890!5m2!1sen!2sin&q=Mahadev+Choraha,+Dibai,+Bulandshahr,+Uttar+Pradesh+203393"
+                src="https://www.google.com/maps?q=Near+Mahadev+Chauraha+Dibai+Bulandshahr+203393&output=embed"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
